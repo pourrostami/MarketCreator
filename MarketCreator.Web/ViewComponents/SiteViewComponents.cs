1fa8cd33cce229134b424f46fc21d49c162cfd44
mohammad <mohammad_pourrostami@yahoo.com>
2024-10-04 14:05:16 +0330

    قسمت قالب سایت

---
+++ MarketCreator.Web/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketCreator.Web.ViewComponents
+{
+
+    #region Site Header
+
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+
+    #endregion
+
+
+    #region Site Footer
+
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+
+    #endregion
+}
